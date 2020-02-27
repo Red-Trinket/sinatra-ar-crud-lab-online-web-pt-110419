f452c5a967bea73ea0add75885f3c6a30827f061
Matthew Jones <matts3371@gmail.com>
2020-02-27 02:19:36 +0000

    Automatically backed up by Learn

--- spec/sinatra_ar_crud_lab_spec.rb
+++ spec/sinatra_ar_crud_lab_spec.rb
@@ -92,7 +92,7 @@ describe "Magazine App" do
       #fill_in :title, :with => "Second Article!!"
       #fill_in :content, :with => "this is the best article ever written"
 
-      page.find(:css, "[type=submit]").click
+      #page.find(:css, "[type=submit]").click
       expect(Article.all.count).to eq(2)
       expect(Article.last.title).to eq("Second Article!!")
     end
